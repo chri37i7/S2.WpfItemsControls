d933f9172553c1479ef5eb7fe3cb1a20ee67bb3f
Christian Alexander Heilmann Bennedbæk <chri37i7@edu.campusvejle.dk>
2020-01-23 12:01:01 +0100

    Made Person class, and started on Repository class.

---
+++ S2.WpfItemsControls.ListBox/Person.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S2.WpfItemsControls.ListBox
+{
+    public class Person
+    {
+        // Fields
+        private string firstname;
+        private string lastname;
+        private string email;
+        private int phoneNumber;
+
+        public Person(string firstname, string lastname, string email, int phoneNumber)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+
+        // Properties
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                Lastname = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+
+        public int PhoneNumber
+        {
+            get
+            {
+                return phoneNumber;
+            }
+            set
+            {
+                phoneNumber = value;
+            }
+        }
+
+        public string FullName => $"{Firstname} {Lastname}";
+    }
+}
---
+++ S2.WpfItemsControls.ListBox/Repository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S2.WpfItemsControls.ListBox
+{
+    public class Repository
+    {
+        private List<Person> persons;
+
+
+    }
+}
