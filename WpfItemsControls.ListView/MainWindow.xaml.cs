077874ea4513dbd2eb5bb194a7b0d4c95e531118
Christian Alexander Heilmann Bennedbæk <chri37i7@edu.campusvejle.dk>
2020-01-29 11:57:34 +0100

    stuff

--- WpfItemsControls.ListView/MainWindow.xaml.cs
+++ WpfItemsControls.ListView/MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace WpfItemsControls.ListView
     public partial class MainWindow : Window
     {
         private ViewModel viewModel;
+        private Repository repository = new Repository();
 
         public MainWindow()
         {
@@ -30,23 +31,11 @@ namespace WpfItemsControls.ListView
             DataContext = viewModel;
         }
 
-        private void ButtonDeleteEmployee_Click(object sender, RoutedEventArgs e)
-        {
-            if(viewModel.SelectedEmployee != null)
-            {
-                viewModel.Employees.Remove(viewModel.SelectedEmployee);
-            }
-            else
-            {
-                MessageBox.Show("Vælg venligst en ansat", "Ingen ansat valgt", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
-        }
-
         private void ButtonNewEmployee_Click(object sender, RoutedEventArgs e)
         {
-            if(viewModel.SelectedEmployee == null)
+            if(textBoxEmployeeFirstname.IsReadOnly != true)
             {
-
+                MessageBox.Show("Husk at tryk gem.", "Fejl!", MessageBoxButton.OK, MessageBoxImage.Error);
             }
             else
             {
@@ -54,20 +43,140 @@ namespace WpfItemsControls.ListView
                 listViewEmployees.SelectedItem = null;
 
                 // Set TextBox BorderThickness
+                datePickerEmploymentDate.BorderThickness = new System.Windows.Thickness(1);
                 textBoxEmployeeFirstname.BorderThickness = new System.Windows.Thickness(1);
                 textBoxEmployeeLastname.BorderThickness = new System.Windows.Thickness(1);
                 textBoxEmployeePosition.BorderThickness = new System.Windows.Thickness(1);
+                textBoxEmployeeSalary.BorderThickness = new System.Windows.Thickness(1);
 
                 // Make TextBoxes Writeable
                 textBoxEmployeeFirstname.IsReadOnly = false;
                 textBoxEmployeeLastname.IsReadOnly = false;
                 textBoxEmployeePosition.IsReadOnly = false;
+                textBoxEmployeeSalary.IsReadOnly = false;
+
+                // Enable DatePicker
+                datePickerEmploymentDate.IsEnabled = true;
             }
         }
 
         private void ButtonEditEmployee_Click(object sender, RoutedEventArgs e)
         {
+            if(viewModel.SelectedEmployee != null)
+            {
+                // Set TextBox BorderThickness
+                datePickerEmploymentDate.BorderThickness = new System.Windows.Thickness(1);
+                textBoxEmployeeFirstname.BorderThickness = new System.Windows.Thickness(1);
+                textBoxEmployeeLastname.BorderThickness = new System.Windows.Thickness(1);
+                textBoxEmployeePosition.BorderThickness = new System.Windows.Thickness(1);
+                textBoxEmployeeSalary.BorderThickness = new System.Windows.Thickness(1);
 
+                // Make TextBoxes Writeable
+                textBoxEmployeeFirstname.IsReadOnly = false;
+                textBoxEmployeeLastname.IsReadOnly = false;
+                textBoxEmployeePosition.IsReadOnly = false;
+                textBoxEmployeeSalary.IsReadOnly = false;
+
+                // Enable DatePicker
+                datePickerEmploymentDate.IsEnabled = true;
+            }
+        }
+
+        private void ButtonSaveEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            if(viewModel.SelectedEmployee == null)
+            {
+                if(textBoxEmployeeFirstname.Text == "" || textBoxEmployeeLastname.Text == "" || textBoxEmployeePosition.Text == "" || textBoxEmployeeSalary.Text == "" || datePickerEmploymentDate.SelectedDate == null)
+                {
+                    if(viewModel.SelectedEmployee == null)
+                    {
+                        MessageBox.Show("Udfyld venligst felterne", "Fejl!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                else
+                {
+                    Employee employee = new Employee(
+                        textBoxEmployeeFirstname.Text,
+                        textBoxEmployeeLastname.Text,
+                        textBoxEmployeePosition.Text,
+                        Convert.ToInt32(textBoxEmployeeSalary.Text),
+                        (datePickerEmploymentDate.SelectedDate ?? DateTime.Now));
+
+                    repository.Add(employee);
+                    viewModel.Employees.Add(employee);
+                    listViewEmployees.SelectedItem = employee;
+                }
+            }
+            else if(viewModel.SelectedEmployee != null)
+            {
+                if(textBoxEmployeeFirstname.Text == "" || textBoxEmployeeLastname.Text == "" || textBoxEmployeePosition.Text == "" || textBoxEmployeeSalary.Text == "" || datePickerEmploymentDate.SelectedDate == null)
+                {
+                    if(viewModel.SelectedEmployee == null)
+                    {
+                        MessageBox.Show("Udfyld venligst felterne", "Fejl!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                else
+                {
+                    repository.ClearFile();
+
+                    viewModel.Employees.Remove(viewModel.SelectedEmployee);
+
+                    foreach(Employee addEmployee in viewModel.Employees)
+                    {
+                        repository.Add(addEmployee);
+                    }
+
+                    Employee employee = new Employee(
+                        "",
+                        "",
+                        "",
+                        0,
+                       (datePickerEmploymentDate.SelectedDate ?? DateTime.Now));
+
+                    repository.Add(employee);
+                    viewModel.Employees.Add(employee);
+                    listViewEmployees.SelectedItem = employee;
+
+                }
+            }
+        }
+
+        private void ButtonDeleteEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            if(viewModel.SelectedEmployee != null)
+            {
+                viewModel.Employees.Remove(viewModel.SelectedEmployee);
+
+                foreach(Employee employee in viewModel.Employees)
+                {
+                    repository.ClearFile();
+                    repository.Add(employee);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vælg venligst en ansat", "Ingen ansat valgt", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void ListViewEmployees_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Set BorderThicknesses
+            datePickerEmploymentDate.BorderThickness = new System.Windows.Thickness(0);
+            textBoxEmployeeFirstname.BorderThickness = new System.Windows.Thickness(0);
+            textBoxEmployeeLastname.BorderThickness = new System.Windows.Thickness(0);
+            textBoxEmployeePosition.BorderThickness = new System.Windows.Thickness(0);
+            textBoxEmployeeSalary.BorderThickness = new System.Windows.Thickness(0);
+
+            // Make TextBoxes Writeable
+            textBoxEmployeeFirstname.IsReadOnly = true;
+            textBoxEmployeeLastname.IsReadOnly = true;
+            textBoxEmployeePosition.IsReadOnly = true;
+            textBoxEmployeeSalary.IsReadOnly = true;
+
+            // Disable DatePicker
+            datePickerEmploymentDate.IsEnabled = false;
         }
     }
 }
--- WpfItemsControls.ListView/Repository.cs
+++ WpfItemsControls.ListView/Repository.cs
@@ -1,21 +1,21 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace WpfItemsControls.ListView
 {
     class Repository
     {
-       private List<Employee> employees;
+        private List<Employee> employees;
+        private string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Employees.txt");
 
         public Repository()
         {
-            employees = new List<Employee>()
-            {
-                new Employee("Brian", "Jørgensen", "Afdelingsleder", 250000, new DateTime(2013, 5, 3)),
-                new Employee("Martin", "Fessel", "Lærer", 1000000, new DateTime(2016, 7, 12)),
-                new Employee("Harry", "Hoe", "Pedel", 50, new DateTime(1947, 11, 12))
-            };
+            employees = new List<Employee>();
+
+            LoadFromFile(path);
         }
 
         public List<Employee> GetAll()
@@ -23,9 +23,92 @@ namespace WpfItemsControls.ListView
             return employees;
         }
 
-        public void add(Employee employee)
+        public void Add(Employee employee)
         {
-            employees.Add(employee);
+            SaveToFile(employee);
         }
+
+        public void ClearFile()
+        {
+            // StreamWriter for writing to file
+            StreamWriter file = new StreamWriter(path, false);
+            // Write Persons to file
+            file.WriteLine();
+            // Close file process
+            file.Close();
+        }
+
+        private void SaveToFile(Employee employee)
+        {
+            // Convert employee to correct format used in the text file.
+            string employeeToText = 
+                $"{employee.Firstname}," +
+                $"{employee.Lastname}," +
+                $"{employee.Position}," +
+                $"{employee.Salary}," +
+                $"{employee.EmploymentDate.ToString("yyyy, MM, dd")}";
+
+            // StreamWriter for writing to file
+            StreamWriter file = new StreamWriter(path, true);
+            // Write Persons to file
+            file.WriteLine(employeeToText);
+            // Close file process
+            file.Close();
+        }
+
+        private void LoadFromFile(string filePath)
+        {
+            if(!File.Exists(filePath))
+            {
+                // Create the file
+                File.Create(filePath);
+
+                // Add empty Employee
+                employees.Add(new Employee(default, default, default, default, default));
+            }
+            else
+            {
+                // StreamReader for reading the document file
+                using(StreamReader reader = new StreamReader(filePath, true))
+                {
+                    // Read until end of the document
+                    while(!reader.EndOfStream)
+                    {
+                        string documentLine;
+                        // Read until end is reached
+                        while((documentLine = reader.ReadLine()) != null)
+                        {
+                            try
+                            {
+                                // Split document lines into array
+                                string[] lineArray = documentLine.Split(",");
+
+                                // TryParse fourth line to int
+                                int.TryParse(lineArray[3], out int lineArrayInt);
+
+                                // Create Employee object and add to list
+                                //
+                                // Format: Firstname, Lastname, Position, Salary, Employment Year, Employment Day, Employment Month
+                                employees.Add(new Employee(
+                                    lineArray[0],
+                                    lineArray[1],
+                                    lineArray[2],
+                                    lineArrayInt,
+                                    new DateTime(
+                                        Convert.ToInt32(lineArray[4]),
+                                        Convert.ToInt32(lineArray[5]),
+                                        Convert.ToInt32(lineArray[6]))));
+                            }
+                            catch(IndexOutOfRangeException)
+                            {
+                                // Catches any empty lines in the file
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
     }
 }
